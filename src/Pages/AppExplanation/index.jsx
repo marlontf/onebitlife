46b1fc8c98aeb3a1cd08a16d31b2e4e06db09683
Marlon Tavares <marlontf@gmail.com>
2022-12-05 00:24:49 -0300

    Expĺanation card finalizado

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,28 +1,62 @@
 import React from "react";
-import { View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
 export default function AppExplanation() {
+   function handleSetShowHome() {
+      console.log("Botão clicado!");
+   }
+
    return (
       <View style={styles.container}>
          <ScrollView>
-            <Text style={styles.title}>
-               Antes, deixa {"\n"} eu te explicar...
-            </Text>
-            <Text style={styles.description}>
-               Pronto(a) para subir de nível na vida?
-            </Text>
-            <Text style={styles.description}>
-               Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de
-               forma individual.
-            </Text>
-            <DefaultButton
-               buttonText={"Continuar"}
-               handlePress={handleSetShowHome}
-               width={250}
-               height={50}
-            />
+            <View style={{ alignItems: "center" }}>
+               <Text style={styles.title}>
+                  Antes, deixa {"\n"} eu te explicar...
+               </Text>
+               <ExplanationCard />
+               <Text style={styles.descriptionCta}>
+                  Pronto(a) para subir de nível na vida?
+               </Text>
+               <Text style={styles.description}>
+                  Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos
+                  de forma individual.
+               </Text>
+               <DefaultButton
+                  buttonText={"Continuar"}
+                  handlePress={handleSetShowHome}
+                  width={250}
+                  height={50}
+               />
+            </View>
          </ScrollView>
       </View>
    );
 }
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: "rgba(21, 21, 21, 0.98)",
+   },
+   title: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "white",
+      textAlign: "center",
+      marginVertical: 40,
+   },
+   descriptionCta: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "white",
+      marginTop: 20,
+      marginBottom: 10,
+   },
+   description: {
+      color: "white",
+      textAlign: "center",
+      marginBottom: 30,
+   },
+});

90cccdbc87d5aade69e60c9cb3d9e79428db3511
Marlon Tavares <marlontf@gmail.com>
2022-12-05 00:54:52 -0300

    Estrutura da página Home e Navegação

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,11 +1,21 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, ScrollView, StyleSheet, Text } from "react-native";
+import {
+   View,
+   ScrollView,
+   StyleSheet,
+   Text,
+   handleNavHome,
+   handleSetShowHome,
+} from "react-native";
 import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
 export default function AppExplanation() {
-   function handleSetShowHome() {
-      console.log("Botão clicado!");
+   const navigation = useNavigation();
+
+   function handleNavHome() {
+      navigation.navigate("Home");
    }
 
    return (
@@ -25,7 +35,7 @@ export default function AppExplanation() {
                </Text>
                <DefaultButton
                   buttonText={"Continuar"}
-                  handlePress={handleSetShowHome}
+                  handlePress={handleNavHome}
                   width={250}
                   height={50}
                />

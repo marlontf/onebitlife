d8dded9738f9965fed4b0113b8eee1f694714603
Marlon Tavares <marlontf@gmail.com>
2022-12-04 23:33:19 -0300

    Inserindo botão padrão com os estilos

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import { useNavigation } from "@react-navigation/native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
+   const handleNavAppExplanation = () => {
+      console.log("Testando o clique");
+   };
+
    return (
       <View style={styles.container}>
          <ScrollView showsVerticalScrollIndicator={false}>
@@ -17,6 +23,13 @@ export default function Start() {
                   Vamos transformar sua vida {"\n"} em jogo, buscando sempre{" "}
                   {"\n"} o melhor nível.
                </Text>
+
+               <DefaultButton
+                  buttonText={"Continuar"}
+                  handlePress={handleNavAppExplanation}
+                  width={250}
+                  height={50}
+               />
             </View>
          </ScrollView>
       </View>
